2891ed6d3e628da2ac2f5755bd6f9e0231ac3cd6
Lotta Anttila <lottaanttila1@hotmail.com>
2026-04-02 10:17:31 +0300

    Fixed joystick

--- cat-game/Code/Joystick/Joystick.cs
+++ cat-game/Code/Joystick/Joystick.cs
@@ -6,41 +6,47 @@ public partial class Joystick : Control
 	[Export] public float HandleLimit = 50f;
 	[Export] public TextureRect _knob;
 	private Vector2 _input = Vector2.Zero;
-	private Vector2 _startPosition;
-	private Vector2 _radius = new Vector2(-1, -1);
+	private Vector2 _inputStartPosition;
+	private Vector2 _knobStartPosition;
 
 	public override void _Ready()
 	{
-		//Dont know how to make this work
-		_startPosition = _knob.Position - _radius;
+		//Set start positions
+		_inputStartPosition = Vector2.Zero;
+		_knobStartPosition = _knob.Position;
 	}
 
 	public override void _GuiInput(InputEvent @event)
 	{
 		if (@event is InputEventScreenTouch touch)
 		{
+			//At first touch be at start
 			if (touch.Pressed)
-				_knob.Position = _startPosition;
+			{
+				_input = _inputStartPosition;
+				_knob.Position = _knobStartPosition;
+			}
+			//At letting go return to start
 			else
 			{
-				_input = Vector2.Zero;
-				_knob.Position = _startPosition;
+				_input = _inputStartPosition;
+				_knob.Position = _knobStartPosition;
 			}
 		}
 		else if (@event is InputEventScreenDrag drag)
 		{
-			Vector2 delta = drag.Position - _startPosition;
+			Vector2 delta = drag.Position - _inputStartPosition;
 			if (delta.Length() > HandleLimit)
 				delta = delta.Normalized() * HandleLimit;
-			_knob.Position = _startPosition + delta;
+			_knob.Position = _knobStartPosition + delta;
 			_input = delta / HandleLimit;
 		}
 		SetDirection();
 	}
 
+	//Give input to Game Manager so it can be taken from there
 	public void SetDirection()
 	{
 		GameManager.Instance.SetPlayerDirection(_input);
-		//GD.Print(_input);
 	}
 }
