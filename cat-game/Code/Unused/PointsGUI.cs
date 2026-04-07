08fd047b0a803c105df835b8f4b45540a9d9c899
Lotta Anttila <lottaanttila1@hotmail.com>
2026-04-07 18:36:07 +0300

    Cleaned up the folders

--- cat-game/Code/Unused/PointsGUI.cs
+++ cat-game/Code/Unused/PointsGUI.cs
@@ -3,9 +3,19 @@ using System;
 
 public partial class PointsGUI : Node
 {
+	private int _islandAmount = 3;
+	private int _npcAmount = 3;
 	// Called when the node enters the scene tree for the first time.
 	public override void _Ready()
 	{
+		for (int i = 0; i < _islandAmount; i++)
+		{
+
+			for (int j = 0; j < _npcAmount; i++)
+			{
+
+			}
+		}
 	}
 
 	// Called every frame. 'delta' is the elapsed time since the previous frame.
